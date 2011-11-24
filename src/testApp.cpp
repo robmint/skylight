edfbd4f20330f9636c42cf46e83170f88c465f71
Robert Carter <rob@mintmedia.co.nz>
2011-11-24 15:39:34 +1300

    add more files

---
+++ src/testApp.cpp
@@ -0,0 +1,57 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+
+
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+	//ofSoundUpdate();
+	//insect.setPan(ofRandomuf());
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+	ofSetColor(0x000000);
+
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+	insect.setPan(ofMap(x, 0, ofGetScreenWidth(), 0, 1));
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
