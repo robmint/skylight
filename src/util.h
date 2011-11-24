17342f8a7495ab4232a1b7b4c565ed1c6b59a9d0
Robert Carter <rob@mintmedia.co.nz>
2011-11-24 16:17:50 +1300

    add utils file with ofxXmlSettings

---
+++ src/util.h
@@ -0,0 +1,11 @@
+/*
+ *  util.h
+ *  skyLight
+ *
+ *  Created by rob on 24/11/11.
+ *  Copyright 2011 createrevolutionaryart.net. All rights reserved.
+ *
+ */
+	#pragma once
+	#include "ofxXmlSettings.h"
+
